7f96be7cc35260375351ae08830a6eb9196f3fba
Artem Udovyk <udovyk@gmail.com>
2022-01-09 21:26:46 +0200

    Revert "Heroku undo"
    This reverts commit 2441f8e040ae47cea859aa3f2e7d3821f0953e8b.

--- config/settings.py
+++ config/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
+
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
